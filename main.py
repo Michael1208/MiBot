8cd66981764b02a6089526e66a7c20bd20d4ec1e
Michael1208 <53876489+Michael1208@users.noreply.github.com>
2019-09-19 11:17:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,17 @@ bot = commands.Bot(command_prefix='mb-')
 async def on_ready():        
     await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name="mb-help"))
     print("MiBot Premium has started!")
+
+@bot.command(pass_context=True)
+@commands.has_permissions(manage_guild=True)
+async def warn(ctx,user:discord.Member,count,*,reason="No Reason Provided"):
+    # get user current warnings
+    try:
+      # add warning
+   except:
+      await ctx.send("Count is Invalid.")
+      raise TypeError # crash out of cmd
+    # send successful message
+     # find server data file
+    # if inexistent, then go and create it.
+    # log the warn count based on a json obj {userid:warncount}
