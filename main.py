eb4e82ecc26aa5072e9e0211186b01f02e5ceb70
Michael1208 <53876489+Michael1208@users.noreply.github.com>
2019-09-19 11:01:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import discord 
+import os
+import asyncio
+import time
+from discord.ext import commands, tasks
+from itertools import cycle
 
+bot = commands.Bot(command_prefix='mb-')
+
+@bot.event
+async def on_ready():        
+    await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name="mb-help"))
+    print("MiBot Premium has started!")
